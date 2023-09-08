b0094b97c6a0ceba746679a096aadb7b443e4857
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-09-08 18:08:32 +0300

    add SQL DB fix

--- src/auth/api/auth.controller.ts
+++ src/auth/api/auth.controller.ts
@@ -10,40 +10,44 @@ import {
   Post,
   Res,
   UnauthorizedException,
-  UseGuards
-} from "@nestjs/common";
-import { CreateUserDto } from "../../users/application/dto/CreateUserDto";
-import { UsersService } from "../../users/application/users.service";
-import { ThrottlerGuard } from "@nestjs/throttler";
-import { RegistrationPipe } from "../pipes/registration.pipe";
-import { CodeDto } from "../../users/application/dto/CodeDto";
-import { EmailDto } from "../../users/application/dto/EmailDto";
-import { NewPasswordDto } from "../application/dto/NewPasswordDto";
-import { LoginDto } from "../application/dto/loginDto";
-import { LocalAuthGuard } from "../guards/local-auth.guard";
-import { CurrentUser } from "../decorators/create-param-current-user.decorator";
-import { SessionDataType } from "./input-model/user-data-request.type";
-import { AuthService } from "../application/auth.service";
-import { Response } from "express";
-import { RefreshJwtAuthGuard } from "../guards/refresh-auth.guard";
-import { CurrentUserRefresh, ParseCurrentRefreshPipe } from "../decorators/create-param-user-refresh.decorator";
-import { UserFromRefreshType } from "./input-model/user-from-refresh.type";
-import { TokenViewModel } from "./view-model/TokenViewModel";
-import { CurrentUserId, ParseCurrentIdDecorator } from "../decorators/create-param-current-id.decarator";
-import { JwtAuthGuard } from "../guards/jwt-auth.guard";
-import { UsersQueryRepository } from "../../users/infrastructure/users.query.repository";
-import { CommandBus } from "@nestjs/cqrs";
-import { CreateTokensCommand } from "../application/use-cases/create-tokens-use-case";
-import { NewTokensCommand } from "../application/use-cases/new-tokens-use-case";
-import { LogoutCommand } from "../application/use-cases/logout-use-case";
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDto } from '../../users/application/dto/CreateUserDto';
+import { UsersService } from '../../users/application/users.service';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { RegistrationPipe } from '../pipes/registration.pipe';
+import { CodeDto } from '../../users/application/dto/CodeDto';
+import { EmailDto } from '../../users/application/dto/EmailDto';
+import { NewPasswordDto } from '../application/dto/NewPasswordDto';
+import { LoginDto } from '../application/dto/loginDto';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { CurrentUser } from '../decorators/create-param-current-user.decorator';
+import { SessionDataType } from './input-model/user-data-request.type';
+import { AuthService } from '../application/auth.service';
+import { Response } from 'express';
+import { RefreshJwtAuthGuard } from '../guards/refresh-auth.guard';
 import {
-  CreateUserByRegistrationCommand
-} from "../../users/application/use-cases/create-user-by-registration-use-case";
-import { ConfirmByEmailCommand } from "../../users/application/use-cases/confirm-by-email-use-case";
-import { EmailResendingCommand } from "../../users/application/use-cases/email -resending-use-case";
-import { RecoveryPasswordCommand } from "../../users/application/use-cases/recovery -password-use-case";
-import { NewPasswordCommand } from "../../users/application/use-cases/new-password-use-case";
-import { IdType } from "../../models/IdType";
+  CurrentUserRefresh,
+  ParseCurrentRefreshPipe,
+} from '../decorators/create-param-user-refresh.decorator';
+import { UserFromRefreshType } from './input-model/user-from-refresh.type';
+import { TokenViewModel } from './view-model/TokenViewModel';
+import {
+  CurrentUserId,
+  ParseCurrentIdDecorator,
+} from '../decorators/create-param-current-id.decarator';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { UsersQueryRepository } from '../../users/infrastructure/users.query.repository';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateTokensCommand } from '../application/use-cases/create-tokens-use-case';
+import { NewTokensCommand } from '../application/use-cases/new-tokens-use-case';
+import { LogoutCommand } from '../application/use-cases/logout-use-case';
+import { CreateUserByRegistrationCommand } from '../../users/application/use-cases/create-user-by-registration-use-case';
+import { ConfirmByEmailCommand } from '../../users/application/use-cases/confirm-by-email-use-case';
+import { EmailResendingCommand } from '../../users/application/use-cases/email -resending-use-case';
+import { RecoveryPasswordCommand } from '../../users/application/use-cases/recovery -password-use-case';
+import { NewPasswordCommand } from '../../users/application/use-cases/new-password-use-case';
+import { IdType } from '../../models/IdType';
 
 @Controller('auth')
 export class AuthController {
