a14fa5ea4580a8940b24e949ee879239b8ad1d0f
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-09-09 16:41:29 +0300

    add SQL DB fix

--- src/users/infrastructure/users.query.repository.ts
+++ src/users/infrastructure/users.query.repository.ts
@@ -166,7 +166,7 @@ WHERE user_id = $1
       `
     SELECT user_id, login, email, password, "createdAt"
 FROM public.users
-WHERE login ILIKE $1 AND email ILIKE $2 AND is_deleted <> true
+WHERE login ILIKE $1 OR email ILIKE $2 AND is_deleted <> true
 ORDER BY "${query.sortBy}" ${query.sortDirection}
 LIMIT $3 OFFSET $4
     `,
