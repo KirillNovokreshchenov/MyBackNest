a3c15cdd71eec52280768f1b4f7ca1339654c912
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-10-31 22:27:19 +0300

    fix typeORM from posts

--- src/posts/infrastructure/postsTypeORM.query.repository.ts
+++ src/posts/infrastructure/postsTypeORM.query.repository.ts
@@ -1,11 +1,8 @@
 import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
-import { IdType } from '../../models/IdType';
-import { RESPONSE_ERROR } from '../../models/RESPONSE_ERROR';
 import { LIKE_STATUS } from '../../models/LikeStatusEnum';
 import { Post } from '../domain/entities-typeorm/post.entity';
 import { PostLike } from '../domain/entities-typeorm/post-like.entity';
-import { PostTypeORMViewType } from '../api/view-types/PostTypeORMViewType';
 import { PostTypeORMViewModel } from '../api/view-models/PostTypeORMViewModel';
 import { NewestLikes } from '../api/view-models/NewestLikeModel';
 import { QueryModel } from '../../models/QueryModel';
@@ -14,7 +11,10 @@ import { PostFilterType } from './types/filter-query.types';
 import { PostViewModelAll } from '../api/view-models/PostViewModelAll';
 import { pagesCount } from '../../helpers/pages-count';
 import { skipPages } from '../../helpers/skip-pages';
-import { PostViewModel } from '../api/view-models/PostViewModel';
+import {
+  PostSQLViewModel,
+  PostViewModel,
+} from '../api/view-models/PostViewModel';
 
 // export class PostsTypeORMQueryRepository {
 //   constructor(
@@ -112,20 +112,48 @@ export class PostsTypeORMQueryRepository {
     @InjectRepository(Post) protected postsRepo: Repository<Post>,
     @InjectRepository(PostLike) protected postLikesRepo: Repository<PostLike>,
   ) {}
+  // Promise<PostViewModel | RESPONSE_ERROR>
+  async findPost(postId: string, userId?: string) {
+    const post = this.postsRepo
+      .createQueryBuilder('p')
+      .leftJoinAndSelect('p.postLikes', 'pl')
+      .getMany();
+    return post;
+  }
+  private async _mapPosts(postsRaw) {
+    const mapPosts: PostSQLViewModel[] = [];
+    const addedPosts = {};
+    for (const postRaw of postsRaw) {
+      let postWithLikes: PostViewModel = addedPosts[postRaw.postId];
+      if (!postWithLikes) {
+        postWithLikes = {
+          id: postRaw.postId,
+          title: postRaw.title,
+          shortDescription: postRaw.shortDescription,
+          content: postRaw.content,
+          blogId: postRaw.blogId,
+          blogName: postRaw.blogName,
+          createdAt: postRaw.createdAt,
+          extendedLikesInfo: {
+            likesCount: Number(postRaw.likesCount),
+            dislikesCount: Number(postRaw.dislikesCount),
+            myStatus: postRaw.myStatus ?? LIKE_STATUS.NONE,
+            newestLikes: [],
+          },
+        };
+        mapPosts.push(postWithLikes);
+        addedPosts[postRaw.postId] = postWithLikes;
+      }
 
-  async findPost(
-    postId: string,
-    userId?: string,
-  ): Promise<PostViewModel | RESPONSE_ERROR> {
-    const post: PostTypeORMViewType | null = await this.postsRepo.findOne({
-      relations: { blog: true },
-      select: { blog: { name: true } },
-      where: { postId: postId, isDeleted: false },
-    });
-    if (!post) return RESPONSE_ERROR.NOT_FOUND;
-    const newestLikes = await this._newestLikes(postId);
-    const myLikeStatus = await this._likeStatus(postId, userId);
-    return new PostTypeORMViewModel(post, newestLikes, myLikeStatus);
+      if (postRaw.like_status) {
+        postWithLikes.extendedLikesInfo.newestLikes.push({
+          userId: postRaw.userId,
+          login: postRaw.login,
+          addedAt: postRaw.addedAt,
+        });
+      }
+    }
+    return mapPosts;
   }
 
   async findAllPost(
