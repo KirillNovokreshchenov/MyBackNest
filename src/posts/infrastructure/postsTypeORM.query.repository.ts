bb56a9f2af65a87c93e6abe20540c921f591d842
KirillNovokreshchenov <kirillnovokrest@gmail.com>
2023-11-06 13:23:40 +0300

    fix functionality for posts by typeORM

--- src/posts/infrastructure/postsTypeORM.query.repository.ts
+++ src/posts/infrastructure/postsTypeORM.query.repository.ts
@@ -15,96 +15,7 @@ import {
   PostSQLViewModel,
   PostViewModel,
 } from '../api/view-models/PostViewModel';
-
-// export class PostsTypeORMQueryRepository {
-//   constructor(
-//     @InjectDataSource() protected dataSource: DataSource,
-//     @InjectRepository(Post) protected postsRepo: Repository<Post>,
-//     @InjectRepository(PostLike) protected postLikesRepo: Repository<PostLike>,
-//   ) {}
-//
-//   async findPost(
-//     postId: string,
-//     userId?: string,
-//   ): Promise<PostViewModel | RESPONSE_ERROR> {
-//     const post: PostTypeORMViewType | null = await this.postsRepo.findOne({
-//       relations: { blog: true },
-//       select: { blog: { name: true } },
-//       where: { postId: postId, isDeleted: false },
-//     });
-//     if (!post) return RESPONSE_ERROR.NOT_FOUND;
-//     const newestLikes = await this._newestLikes(postId);
-//     const myLikeStatus = await this._likeStatus(postId, userId);
-//     return new PostTypeORMViewModel(post, newestLikes, myLikeStatus);
-//   }
-//
-//   async findAllPost(
-//     dataQuery: QueryInputType,
-//     postFilter: PostFilterType,
-//   ): Promise<any> {
-//     const query = new QueryModel(dataQuery);
-//     let condition = {};
-//     if (postFilter.blogId) {
-//       condition = { blogId: postFilter.blogId };
-//     }
-//     const skip = skipPages(query.pageNumber, query.pageSize);
-//     const [allPosts, totalCount] = await this.postsRepo.findAndCount({
-//       relations: { blog: true },
-//       select: { blog: { name: true } },
-//       order: {
-//         [query.sortBy]: query.sortDirection,
-//       },
-//       where: condition,
-//       skip: skip,
-//       take: query.pageSize,
-//     });
-//     const countPages = pagesCount(totalCount, query.pageSize);
-//
-//     const mapPosts = await Promise.all(
-//       allPosts.map(async (post) => {
-//         const newestLikes = await this._newestLikes(post.postId);
-//         const myLikeStatus = await this._likeStatus(
-//           post.postId,
-//           postFilter.userId as string,
-//         );
-//         return new PostTypeORMViewModel(post, newestLikes, myLikeStatus);
-//       }),
-//     );
-//
-//     return new PostViewModelAll(
-//       countPages,
-//       query.pageNumber,
-//       query.pageSize,
-//       totalCount,
-//       mapPosts,
-//     );
-//   }
-//   async _newestLikes(postId: string): Promise<NewestLikes[]> {
-//     const newestLikes = await this.postLikesRepo.find({
-//       relations: { user: true },
-//       select: { user: { login: true } },
-//       where: { postId: postId, likeStatus: LIKE_STATUS.LIKE },
-//       order: { createdAt: 'DESC' },
-//       take: 3,
-//     });
-//     return newestLikes.map((like) => {
-//       return {
-//         userId: like.ownerId,
-//         login: like.user.login,
-//         addedAt: like.createdAt,
-//       };
-//     });
-//   }
-//
-//   async _likeStatus(postId: string, userId?: string) {
-//     if (userId) {
-//       const like = await this.postLikesRepo.findOne({
-//         where: { postId: postId, ownerId: userId },
-//       });
-//       return like?.likeStatus;
-//     }
-//   }
-// }
+import { RESPONSE_ERROR } from '../../models/RESPONSE_ERROR';
 
 export class PostsTypeORMQueryRepository {
   constructor(
@@ -114,47 +25,70 @@ export class PostsTypeORMQueryRepository {
   ) {}
   // Promise<PostViewModel | RESPONSE_ERROR>
   async findPost(postId: string, userId?: string) {
-    const post = this.postsRepo
-      .createQueryBuilder('p')
-      // .addSelect((sq) => {
-      //   return sq
-      //     .select('count(*)', 'totalCount')
-      //     .from('post', 'p')
-      //     .where('p.blogId = COALESCE(:blogId, p.blogId)', { blogId: blogId });
-      // })
-
+    const rawPost = await this.dataSource
+      .createQueryBuilder()
+      .select('"p".*')
+      .from((subQuery) => {
+        return subQuery
+          .from('post', 'p')
+          .where('"postId" = :postId', { postId: postId })
+          .andWhere('"isDeleted" <> true');
+      }, 'p')
+      .addSelect((sq) => {
+        return sq
+          .select('count(*)', 'likesCount')
+          .from('post_like', 'pl')
+          .where("pl.likeStatus = 'Like'")
+          .andWhere('"pl"."postId" = "p"."postId"');
+      })
+      .addSelect((sq) => {
+        return sq
+          .select('count(*)', 'dislikesCount')
+          .from('post_like', 'pl')
+          .where("pl.likeStatus = 'Dislike'")
+          .andWhere('pl."postId" = "p"."postId"');
+      })
+      .addSelect((sq) => {
+        return sq
+          .select('pl.likeStatus', 'myStatus')
+          .from('post_like', 'pl')
+          .where('"pl"."postId" = "p"."postId"')
+          .andWhere('"pl"."ownerId" = :ownerId', {
+            ownerId: userId,
+          });
+      })
+      .addSelect('name')
+      .leftJoin('blog', 'b', '"b"."blogId" = "p"."blogId"')
       .leftJoinAndSelect(
         (sq) => {
           return sq
-            .select()
+            .select(
+              '"postId", "pl"."createdAt" as "addedAt", "likeId", "likeStatus", "login", "ownerId" as "userId"',
+            )
             .from('post_like', 'pl')
+            .leftJoin('pl.user', 'u')
             .where((qb) => {
               const subQuery = qb
                 .subQuery()
                 .select('l.likeId')
                 .from('post_like', 'l')
-                .where("pl.likeStatus = 'Like'")
+                .where("l.likeStatus = 'Like'")
                 .andWhere('l.postId = pl.postId')
                 .orderBy({ 'l.createdAt': 'DESC' })
                 .limit(3)
                 .getQuery();
               return 'pl.likeId IN ' + subQuery;
-            })
-            .orderBy({ 'pl.createdAt': 'DESC' });
+            });
         },
-        'newestLikes',
-        '"newestLikes"."postId" = p.postId',
+        'nl',
+        '"nl"."postId" = "p"."postId"',
       )
-      .orderBy()
-      // .leftJoin('p.blog', 'b')
-      .getSql();
-
-    return post;
-    // .addSelect('b.name', 'blogName')
-    // .where('p.blogId = COALESCE(:blogId, p.blogId)', { blogId: blogId })
+      .getRawMany();
+    if (!rawPost[0]) return RESPONSE_ERROR.NOT_FOUND;
+    return this._mapPosts(rawPost)[0];
   }
-  private async _mapPosts(postsRaw) {
-    const mapPosts: PostSQLViewModel[] = [];
+  private _mapPosts(postsRaw) {
+    const mapPosts: PostViewModel[] = [];
     const addedPosts = {};
     for (const postRaw of postsRaw) {
       let postWithLikes: PostViewModel = addedPosts[postRaw.postId];
@@ -165,7 +99,7 @@ export class PostsTypeORMQueryRepository {
           shortDescription: postRaw.shortDescription,
           content: postRaw.content,
           blogId: postRaw.blogId,
-          blogName: postRaw.blogName,
+          blogName: postRaw.name,
           createdAt: postRaw.createdAt,
           extendedLikesInfo: {
             likesCount: Number(postRaw.likesCount),
@@ -178,8 +112,8 @@ export class PostsTypeORMQueryRepository {
         addedPosts[postRaw.postId] = postWithLikes;
       }
 
-      if (postRaw.like_status) {
-        postWithLikes.extendedLikesInfo.newestLikes.push({
+      if (postRaw.likeStatus) {
+        postWithLikes.extendedLikesInfo.newestLikes.unshift({
           userId: postRaw.userId,
           login: postRaw.login,
           addedAt: postRaw.addedAt,
@@ -195,7 +129,7 @@ export class PostsTypeORMQueryRepository {
   ): Promise<any> {
     const query = new QueryModel(dataQuery);
     const skip = skipPages(query.pageNumber, query.pageSize);
-    const post = this.dataSource
+    const rawPosts = await this.dataSource
       .createQueryBuilder()
       .select('"p".*')
       .from((subQuery) => {
@@ -208,6 +142,7 @@ export class PostsTypeORMQueryRepository {
             ['"p".' + `"` + `${query.sortBy}` + `"`]:
               query.sortDirection.toUpperCase() === 'DESC' ? 'DESC' : 'ASC',
           })
+          .andWhere('"isDeleted" <> true')
           .skip(skip)
           .take(query.pageSize);
       }, 'p')
@@ -221,14 +156,14 @@ export class PostsTypeORMQueryRepository {
       })
       .addSelect((sq) => {
         return sq
-          .select('count(*)', 'likeCount')
+          .select('count(*)', 'likesCount')
           .from('post_like', 'pl')
           .where("pl.likeStatus = 'Like'")
           .andWhere('"pl"."postId" = "p"."postId"');
       })
       .addSelect((sq) => {
         return sq
-          .select('count(*)', 'dislikeCount')
+          .select('count(*)', 'dislikesCount')
           .from('post_like', 'pl')
           .where("pl.likeStatus = 'Dislike'")
           .andWhere('pl."postId" = "p"."postId"');
@@ -242,13 +177,14 @@ export class PostsTypeORMQueryRepository {
             ownerId: postFilter.userId,
           });
       })
-      .addSelect('b.name')
+      .addSelect('name')
       .leftJoin('blog', 'b', '"b"."blogId" = "p"."blogId"')
       .leftJoinAndSelect(
         (sq) => {
           return sq
-            .select('"pl".*')
-            .addSelect('u.login')
+            .select(
+              '"postId", "pl"."createdAt" as "addedAt", "likeId", "likeStatus", "login", "ownerId" as "userId"',
+            )
             .from('post_like', 'pl')
             .leftJoin('pl.user', 'u')
             .where((qb) => {
@@ -275,16 +211,16 @@ export class PostsTypeORMQueryRepository {
           query.sortDirection.toUpperCase() === 'DESC' ? 'DESC' : 'ASC',
       })
       .getRawMany();
-    return post;
+    const mapPosts = this._mapPosts(rawPosts);
+    const totalCount = Number(rawPosts[0].totalCount) ?? 0;
+    const countPages = pagesCount(totalCount, query.pageSize);
 
-    // const countPages = pagesCount(totalCount, query.pageSize);
-
-    // return new PostViewModelAll(
-    //   countPages,
-    //   query.pageNumber,
-    //   query.pageSize,
-    //   totalCount,
-    //   mapPosts,
-    // );
+    return new PostViewModelAll(
+      countPages,
+      query.pageNumber,
+      query.pageSize,
+      totalCount,
+      mapPosts,
+    );
   }
 }
